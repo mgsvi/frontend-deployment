bfbafcf97c4051ecba77db20f2e152280877ffd2
gowtham321 <gowthamsaravan1@gmail.com>
2023-11-08 09:34:49 +0530

    update import on asset create

--- app/Assets/create/page.js
+++ app/Assets/create/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { React, useState, useEffect, useMemo } from "react";
+import { React, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { v4 as uuidv4 } from 'uuid';
 import {
